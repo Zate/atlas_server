eb05dace700695191fe8d955f5a5ff5b5400500d
Zate <zate75@gmail.com>
2019-01-23 09:36:50 -0600

    merging server update and map update

--- atlas/update_atlas.sh
+++ atlas/update_atlas.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
 ME=`whoami`
-export ATLAS_HOME=${me}/.atlas
-export STEAMCMD=~/.steamcmd
+ATLAS_HOME=${me}/.atlas
+STEAMCMD=~/.steamcmd
+SHOOTERGAME="${ATLAS_HOME}/ShooterGame"
 
 cd $STEAMCMD
 ./steamcmd.sh +login anonymous +force_install_dir $ATLAS_HOME +app_update 1006030 validate +quit
-setfacl -Rm u:10000:rwx ${ATLAS_HOME}
+
+if [ -d "$SHOOTERGAME" ]; then
+    cd $SHOOTERGAME
+    pwd
+    if [ ! -d "$SHOOTERGAME/ServerGrid" ]; then
+        mkdir -p $SHOOTERGAME/ServerGrid
+    fi 
+    wget -q https://raw.githubusercontent.com/Zate/atlas_server/master/atlas/ServerGrid.json
+    wget -q https://raw.githubusercontent.com/Zate/atlas_server/master/atlas/ServerGrid.ServerOnly.json
+    wget -q https://github.com/Zate/atlas_server/raw/master/atlas/ServerGrid/CellImg_0-0.jpg -O $SHOOTERGAME/ServerGrid/CellImg_0-0.jpg
+    wget -q https://github.com/Zate/atlas_server/raw/master/atlas/ServerGrid/MapImg.jpg -O $SHOOTERGAME/ServerGrid/MapImg.jpg
+    setfacl -Rm u:10000:rwx ${ATLAS_HOME}
+    exit 1
+fi
+echo "$SHOOTERGAME missing, check update_server.sh has run correctly."
