9d5c20b707ef48b58c55ad8e7b8424a945c39ddb
Zate <zate75@gmail.com>
2019-01-22 20:56:57 -0600

    trying to unfuck the ports

--- atlas/start_server.sh
+++ atlas/start_server.sh
@@ -10,5 +10,5 @@ cd /home/steam/.atlas/ShooterGame/Binaries/Linux/
 #./ShooterGameServer "Ocean?ServerX=0?ServerY=0?AltSaveDirectoryName=A1?MaxPlayers=10?ReservedPlayerSlots=0?SeamlessIP=$IP?Port=5701?QueryPort=57501?RconPassword=test -NoBattlEye -log -server"
 
 # Ocean?ServerX=0?ServerY=0?MaxPlayers=40?Port=27018?QueryPort=27019?SeamlessIP=64.94.95.61?ServerAdminPassword=ne14t3nni5?RCONEnabled=True?RCONPort=27020?ReservedPlayerSlots=0 -log -server -ForceAllHomeServer -NoSeamlessServer
-./ShooterGameServer "Ocean?ServerX=0?ServerY=0?MaxPlayers=10?Port=27018?QueryPort=27019?SeamlessIP=159.203.52.169?ServerAdminPassword=changeme?RCONEnabled=True?RCONPort=27021?ReservedPlayerSlots=0 -log -server -ForceAllHomeServer -allcores -servergamelog -forcenetthreading"
+./ShooterGameServer "Ocean?ServerX=0?ServerY=0?MaxPlayers=10?Port=27005?QueryPort=27015?SeamlessIP=159.203.52.169?ServerAdminPassword=changeme?RCONEnabled=True?RCONPort=27025?ReservedPlayerSlots=0 -log -server -ForceAllHomeServer"
 # change ports to this Port=27018?QueryPort=27019?
