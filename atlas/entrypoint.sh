da8465adba147c60bdc1d66ca2b78110909e790a
Zate <zate75@gmail.com>
2019-01-24 16:13:42 +0000

    adding tail dev null to try and keep the server up/running

--- atlas/entrypoint.sh
+++ atlas/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 if [ "$1" = 'atlas' ]; then
-    exec /usr/bin/screen -dmS atlas /home/${USER}/bin/start_server.sh
+    exec /usr/bin/screen -dmS atlas /home/${USER}/bin/start_server.sh && tail -f /dev/null
 fi
 exec "$@"
