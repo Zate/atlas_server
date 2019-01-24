f11276eed9770b3235f41c21598817c7e9facb0c
Zate <zate75@gmail.com>
2019-01-24 15:52:34 +0000

    adding new entrypoint and hopefully screen launch

--- atlas/entrypoint.sh
+++ atlas/entrypoint.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 set -e
-#"${VARIABLE:=DEFAULT_VALUE}"
-MYIP=`wget -qO - https://ifconfig.me`
-"${SIP:=MYIP}"
-"${MAXPLAYERS:='10'}"
-"${GAMEPORT:='27005'}"
-"${QUERYPORT:='27015'}"
-"${ADMINPASS:='changeme'}"
-"${RCON:='True'}"
-"${RCONPORT:='27025'}"
-"${RESPLAYERS:='0'}"
-"${SLOG:='-log'}"
-"${ALLHOME:='-ForceAllHomeServer'}"
-"${MAP:='Ocean'}"
-"${SVRX:='0'}"
-"${SVRY:='0'}"
-cd /home/steam/.atlas/ShooterGame/Binaries/Linux/
-# if [ "$1" = 'update' ]; then
-    
-# fi
-
-exec ./ShooterGameServer Ocean?ServerX=0?ServerY=0?MaxPlayers=10?Port=27005?QueryPort=27015?SeamlessIP=${SIP}?ServerAdminPassword=changeme?RCONEnabled=True?RCONPort=27025?ReservedPlayerSlots=0 -log -server -ForceAllHomeServer
+if [ "$1" = 'atlas' ]; then
+    exec /usr/bin/screen -dmS atlas /home/${USER}/bin/start_server.sh
+fi
+exec "$@"
