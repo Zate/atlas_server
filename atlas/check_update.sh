bec7dc9627b66c744be956fdbb0db2bfee99863d
Zate <zate75@gmail.com>
2019-01-30 19:37:50 +0000

    adding websocket support for local vmstats server

--- atlas/check_update.sh
+++ atlas/check_update.sh
@@ -11,7 +11,7 @@ rm -rf /home/${USER}/Steam/appcache/appinfo.vdf
 LOCALBID=`cat ${ATLAS_HOME}/steamapps/appmanifest_1006030.acf | grep buildid | cut -d'"' -f4`
 #        "buildid"               "3497453"
         
-REMOTEBID=`${STEAMCMD}/steamcmd.sh +app_info_update 1 +app_info_print 1006030 +quit | grep buildid | cut -d'"' -f4`
+REMOTEBID=`${STEAMCMD}/steamcmd.sh +login anonymous +app_info_update 1 +app_info_print 1006030 +quit | grep buildid | cut -d'"' -f4`
 #                                "buildid"               "3497453"
 if [ "$LOCALBID" == "$REMOTEBID" ]; then
     echo "No update available - L : ${LOCALBID} R: ${REMOTEBID}"
--- atlas/update_atlas.sh
+++ atlas/update_atlas.sh
@@ -11,14 +11,14 @@ rm -rf /home/${USER}/Steam/appcache/appinfo.vdf
 LOCALBID=`cat ${ATLAS_HOME}/steamapps/appmanifest_1006030.acf | grep buildid | cut -d'"' -f4`
 #        "buildid"               "3497453"
         
-REMOTEBID=`${STEAMCMD}/steamcmd.sh +app_info_update 1 +app_info_print 1006030 +quit | grep buildid | cut -d'"' -f4`
+REMOTEBID=`${STEAMCMD}/steamcmd.sh +login anonymous +app_info_update 1 +app_info_print 1006030 +quit | grep buildid | cut -d'"' -f4`
 #                                "buildid"               "3497453"
 if [ "$LOCALBID" == "$REMOTEBID" ]; then
     echo "No update available"
     exit 1
 fi
 
-echo "Update Availablr .."
+echo "Update Available .."
 
 cd $STEAMCMD
 sudo setfacl -Rm u:10000:rwx ${ATLAS_HOME}
